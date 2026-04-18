ccf83272ab3147b118cd024f4404927c9da390f4
jcomcl <j@jcom.cl>
2026-04-18 21:53:58 +0100

    first pass at dialogue

--- ui/dialogue_panel.gd
+++ ui/dialogue_panel.gd
@@ -1,10 +1,14 @@
 @tool
+class_name DialoguePanel
 extends Control
 
 @export var width = 200
 @export var width_ratio = 0.4
+@export var speaker_color: Color = Color.WHITE
 
 @onready var timeline = $"ScrollContainer/VBoxContainer"
+
+signal response(code)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,13 +20,19 @@ func _process(delta: float) -> void:
 	size.x = min(width, get_parent().size.x * width_ratio)
 	position.x = get_parent().size.x - size.x
 
-func show_text(s:String):
-	visible = true
-	$Label.text = s
-
 const textbox_scn = preload("res://ui/textbox.tscn")
-func add_description_box(s:String):
+func add_box(s:String) -> TextBox:
+	visible = true
 	var tbox = textbox_scn.instantiate()
-	tbox.text = "[i]%s[/i]" % s
 	timeline.add_child(tbox)
+	tbox.text = s
+	return tbox
+
+func add_description_box(s:String) ->TextBox:
+	s = "[i]%s[/i]" % s
+	return add_box(s)
+
+func add_speech_box(s:String) -> TextBox:
+	s ="[color=#%s]%s[/color]" % [speaker_color.to_html(), s]
+	return add_box(s)
 	
--- ui/textbox.gd
+++ ui/textbox.gd
@@ -1,23 +1,32 @@
 @tool
+class_name TextBox
 extends Control
+
+signal done_showing
 
 @export var text:String:
 	set(s):
 		text=s
-		if is_node_ready():
-			get_label().text = s
-	get():
-		return get_label().text
+		if not is_node_ready():
+			await ready
+		get_label().text = s
+		get_label().visible_characters = 0
 
-func get_label()->RichTextLabel: 
+func get_label()->RichTextLabel:
 	return get_child(0).get_child(0)
-# Called when the node enters the scene tree for the first time.
+
+var text_reveal_cumer = Cumer.new(40)
 func _ready() -> void:
-	pass # Replace with function body.
+	text=text
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if get_label().visible_characters < text.length():
+		text_reveal_cumer.add(delta)
+		get_label().visible_characters = text_reveal_cumer.i
+		if get_label().visible_characters >= text.length():
+			done_showing.emit()
+
+
 	var n = get_label()
 	if n is RichTextLabel:
 		custom_minimum_size.y = n.size.y
