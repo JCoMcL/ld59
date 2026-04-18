6199eabb1c1de32230de9c8b3facca02fd0e1026
jcomcl <j@jcom.cl>
2026-04-18 12:27:24 +0100

    first stuff

---
+++ dialogue_panel.gd
@@ -0,0 +1,16 @@
+@tool
+extends Control
+
+@export var height = 200
+@export var height_ratio = 0.4
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	size.y = min(height, get_parent().size.y * height_ratio)
+	position.y = get_parent().size.y - size.y
+	
