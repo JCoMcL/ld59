7668d4660dfd743d18cc6c93418c8d15c62b2d4b
jcomcl <j@jcom.cl>
2026-04-18 23:04:58 +0100

    add nicer dialogue with character portrait

--- characters/character.gd
+++ characters/character.gd
@@ -1,21 +1,21 @@
 extends TextureRect
 
 @export var speaker_color:Color
-
+@export var speaker_portrait:Texture2D
 
 func open_dialogue():
 	var p = Root.get_dialogue_panel(self)
+	print(p)
+	p.clear()
 	p.speaker_color = speaker_color
+	p.set_speaker_portrait(speaker_portrait)
 	await p.add_description_box(
-"A murky old man rests against a cold brick. His face reminds you of an every single alcoholic
-human misfortune you ever saw. His teeth are pereodically clinging on an empty bottle of
-cheap beer in a desperate attempt to extract a nostalgic flavor. It feels like the bottle
-sucks his life out."
+"A murky old man rests against a cold brick. His face reminds you of an every single alcoholic human misfortune you ever saw. His teeth are pereodically clinging on an empty bottle of cheap beer in a desperate attempt to extract a nostalgic flavor. It feels like the bottle sucks his life out."
 	).done_showing
 	await p.add_description_box(
 "A torn-off voice is reaching you through his mouth, the rest of his body appears almost immobile."
 	).done_showing
-	await p.add_speech_box("Ah, what did [i]you[/i] do").done_showing
+	await p.add_speech_box("Ah... what did [i]you[/i] do?").done_showing
 
 
 func _gui_input(ev: InputEvent):
--- ui/dialogue_panel.gd
+++ ui/dialogue_panel.gd
@@ -8,8 +8,6 @@ extends Control
 
 @onready var timeline = $"ScrollContainer/VBoxContainer"
 
-signal response(code)
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = timeline.get_child_count() > 0
@@ -19,6 +17,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	size.x = min(width, get_parent().size.x * width_ratio)
 	position.x = get_parent().size.x - size.x
+
+func clear():
+	for c in timeline.get_children():
+		c.free()
+
+func set_speaker_portrait(tex:Texture2D):
+	if not tex:
+		return
+	%SpeakerPortrait.texture = tex
+	%SpeakerPortrait.position.x = -tex.get_size().x -10 
 
 const textbox_scn = preload("res://ui/textbox.tscn")
 func add_box(s:String) -> TextBox:
